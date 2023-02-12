836f9b98693563187d69ee3e30ca3ee00ba723bf
a.levashov <a.levashov@gmail.com>
2023-02-12 20:38:00 +0300

    првиет

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hello')
+    print('Привет')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
